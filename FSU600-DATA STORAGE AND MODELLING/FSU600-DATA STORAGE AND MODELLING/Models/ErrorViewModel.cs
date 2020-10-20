6d941061f66f07d89ce3e7bfbd2a9b8866f19d2f
Samira Abdoljavadi <saab0029@student.hv.se>
2020-10-21 00:19:45 +0200

    Create Project

---
+++ FSU600-DATA STORAGE AND MODELLING/FSU600-DATA STORAGE AND MODELLING/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FSU600_DATA_STORAGE_AND_MODELLING.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
