6d8b1058845183e7133af4ab21d7ab7bd3cfce4b
Samira Abdoljavadi <saab0029@student.hv.se>
2020-10-21 14:12:34 +0200

    Create ApartmentQueuing Controller

--- FSU600-DATA STORAGE AND MODELLING/FSU600-DATA STORAGE AND MODELLING/Program.cs
+++ FSU600-DATA STORAGE AND MODELLING/FSU600-DATA STORAGE AND MODELLING/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace FSU600_DATA_STORAGE_AND_MODELLING
 {
@@ -14,6 +16,16 @@ namespace FSU600_DATA_STORAGE_AND_MODELLING
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
+            var dbClient = new MongoClient("mongodb://127.0.0.1:27017");
+            var dbList = dbClient.ListDatabases().ToList();
+
+            Console.WriteLine("The list of databases are:");
+
+            foreach (var item in dbList)
+            {
+                Console.WriteLine(item);
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
